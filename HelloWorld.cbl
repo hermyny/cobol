74974f2f4d4f3ef041b38c49cb13e179ef17a186
hermyny <hermance18@gmail.com>
2024-09-02 21:25:49 +0200

    first commit

---
+++ HelloWorld.cbl
@@ -0,0 +1,14 @@
+       program-id. HelloWorld.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       linkage section.
+
+       procedure division.
+           DISPLAY "Hello My Friends!! LA vie est belle!!"
+           goback.
+
+       end program HelloWorld.
